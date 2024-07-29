220728bb550fea598b38f869ed78defa7dcd714f
Fungus22 <dzakynugrohoguen12@gmail.com>
2024-07-29 14:33:25 +0700

    Update 2024_07_26_074636_table_varian.php

--- database/migrations/2024_07_26_074636_table_varian.php
+++ database/migrations/2024_07_26_074636_table_varian.php
@@ -20,6 +20,6 @@ return new class extends Migration
     
     public function down(): void
     {
-        Schema::dropIfExist("parasit.varian");
+        Schema::dropIfExists("parasit.varian");
     }
 };
